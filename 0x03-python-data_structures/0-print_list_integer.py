358bae4b50a314840ea47e96de79410f254b2c05
jundi yusuf <jundiyusuf10@gmail.com>
2024-03-16 14:00:20 -0700

    question1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Prints each integer from the given list on a separate line.
+
+    Args:
+        my_list (list): List containing integers.
+
+    Returns:
+        None
+    """
+    for num in my_list:
+        print(num)
