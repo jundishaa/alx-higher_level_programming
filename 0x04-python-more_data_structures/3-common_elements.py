21eb22e58dfc4e27285a3c97a716029936c5f8f4
jundi yusuf <jundiyusuf10@gmail.com>
2024-03-20 17:28:07 -0700

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+
+    Args:
+        set_1: The first set.
+        set_2: The second set.
+
+    Returns:
+        A set containing the common elements between set_1 and set_2.
+    """
+
+    common_elements = set_1.intersection(set_2)  # Use the intersection method efficiently
+    return common_elements
