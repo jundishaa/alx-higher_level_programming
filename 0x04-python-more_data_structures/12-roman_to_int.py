974c30cf345526ab47fd7c3c1fdcab214fc3ea72
jundi yusuf <jundiyusuf10@gmail.com>
2024-03-20 18:19:53 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    """
+    Converts a Roman numeral to an integer.
+
+    Args:
+        roman_string: The Roman numeral string to convert.
+
+    Returns:
+        The integer value of the Roman numeral, or 0 if invalid input.
+    """
+
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
     roman_numerals = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
 
-    integer_value = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_numerals[roman_string[i]] > roman_numerals[roman_string[i - 1]]:
-            integer_value += roman_numerals[roman_string[i]] - 2 * roman_numerals[roman_string[i - 1]]
+    result = 0
+    i = 0
+    while i < len(roman_string):
+        current_char = roman_string[i]
+        next_char = roman_string[i + 1] if i + 1 < len(roman_string) else None
+
+        if next_char is not None and roman_numerals[current_char] < roman_numerals[next_char]:
+            result += roman_numerals[next_char] - roman_numerals[current_char]
+            i += 2
         else:
-            integer_value += roman_numerals[roman_string[i]]
-    return integer_value
+            result += roman_numerals[current_char]
+            i += 1
+
+    return result
