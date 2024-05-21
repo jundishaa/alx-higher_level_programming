7418f035a2021d543d6a9ee65df24eaa97f0e555
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-21 06:32:46 -0700

    querry

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,10 @@
--- Script to print the full description of the table first_table
-
--- Query the information_schema to get the table structure
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Print the full description of the table first_table from the database hbtn_0c_0
+SELECT COLUMN_NAME AS Field,
+       DATA_TYPE AS Type,
+       IS_NULLABLE AS 'Null',
+       COLUMN_KEY AS 'Key',
+       COLUMN_DEFAULT AS Default,
+       EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+  AND TABLE_NAME = 'first_table';
