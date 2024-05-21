693502214cab78a5c247bc870369962f5006d994
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-21 06:36:45 -0700

    querry

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,10 +1,2 @@
 -- Print the full description of the table first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME AS Field,
-       DATA_TYPE AS Type,
-       IS_NULLABLE AS 'Null',
-       COLUMN_KEY AS 'Key',
-       COLUMN_DEFAULT AS Default,
-       EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-  AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
