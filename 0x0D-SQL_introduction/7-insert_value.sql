bacb1519c86aa464ff33b0b58de1d6b2db77724b
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-21 06:48:18 -0700

    querry

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Script to insert a new row into the table first_table in the database hbtn_0c_0
+-- New row: id = 89, name = 'Best School'
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
