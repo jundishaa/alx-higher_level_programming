b86717c02679bf5d1b7ceccf649d3949434c9c55
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-23 06:19:00 -0700

    sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,10 @@
--- Script to list all shows from hbtn_0d_tvshows_rate by their rating
--- File: 102-rating_shows.sql
+-- Script to list all genres from hbtn_0d_tvshows_rate and display the number of shows linked to each
+-- File: 13-count_shows_by_genre.sql
 
-SELECT tv_shows.title, SUM(rating) AS rating_sum
-FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_genres.name
+HAVING COUNT(tv_shows.title) > 0
+ORDER BY number_of_shows DESC;
