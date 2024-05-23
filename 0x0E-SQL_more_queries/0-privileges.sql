de5a3ba75717fce98f02b40b6f17dd70c6ab3aaa
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-23 05:25:21 -0700

    sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,10 @@
+-- Script to list all privileges for user_0d_1 and user_0d_2
+-- File: 0-privileges.sql
+
+-- List privileges for user_0d_1
+-- Query to show privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+-- Query to show privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
