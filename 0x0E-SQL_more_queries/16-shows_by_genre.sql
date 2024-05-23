abcc53c25ea47d897febd470bdde60cbd3f6b630
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-23 06:14:34 -0700

    sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,10 +1,8 @@
--- Script to list all shows and their linked genres from the hbtn_0d_tvshows database
+-- Script to list all shows and all genres linked to each show from hbtn_0d_tvshows
 -- File: 16-shows_by_genre.sql
 
--- Select all shows and their linked genres
-SELECT tv_shows.title, GROUP_CONCAT(tv_genres.name ORDER BY tv_genres.name ASC SEPARATOR ', ') AS name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC, name ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
