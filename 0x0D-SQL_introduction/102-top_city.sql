cb2ba7a24e909666e8b1963aadd69231a3b224ea
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-23 06:48:20 -0700

    sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,12 @@
--- Script to display the top 3 cities' temperatures during July and August ordered by temperature (descending)
+-- Task: Write a SQL script to display the top 3 cities' temperatures during July and August ordered by temperature (descending)
 
--- Calculate temperature in Fahrenheit for July and August, then select the top 3 cities
-SELECT city, MAX(temperature * 9 / 5 + 32) AS max_temperature_fahrenheit
-FROM imported_table_name
+-- Selecting the city and calculating the average temperature for each city during July and August from the temperatures table
+-- Filtering the data for July and August using the MONTH function
+-- Ordering the results by the average temperature in descending order
+-- Limiting the output to the top 3 cities
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperatures
 WHERE MONTH(date) IN (7, 8)
 GROUP BY city
-ORDER BY max_temperature_fahrenheit DESC
+ORDER BY avg_temp DESC
 LIMIT 3;
